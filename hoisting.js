69f6fef315c08d12c748904f2193a3fe2c0e34d3
vinod pal <palvinodtws94@gmail.com>
2023-09-10 15:12:36 +0530

    test code

---
+++ hoisting.js
@@ -0,0 +1,38 @@
+
+// function getName(a,b,c, ...other){
+//     return a+b+c
+// }
+// let sum=getName(1,2,3,4,5,6,7,8)
+// console.log("sum-->",sum)
+
+// const names=["ajay","ravi","arjun","shiva"]
+// function getName(name1,name2,name3,name4){
+//     console.log(name1,name2,name3,name4)
+// }
+// getName(...names)
+// getName(names)
+
+// rest with object
+
+// const student = {
+//     name:"ajay",
+//     age:28,
+//     hobbies:["cricket","singing"]
+// }
+// // let {age} = student
+// // let {age,...rest} =student
+// let {...rest} =student
+// console.log(rest)
+
+// spread with object 
+
+// const student = {
+//         name:"ajay",
+//         age:28,
+//         hobbies:["cricket","singing"]
+//     }
+// const newStudent = {
+//     ...student,
+//     age:29
+// }
+// console.log(newStudent)
---
+++ test.js
@@ -0,0 +1,73 @@
+
+
+
+const users=[
+    {
+        id:1,
+        name:"arun",
+        age:25,
+        isActive:true
+    },
+    {
+        id:2,
+        name:"nitin",
+        age:15,
+        isActive:true
+    },
+    {
+        id:3,
+        name:"ravi",
+        age:27,
+        isActive:false
+    },
+    {
+        id:4,
+        name:"vijay",
+        age:21,
+        isActive:true
+    },
+    {
+        id:5,
+        name:"jatin",
+        age:10,
+        isActive:true
+    }
+]
+
+// solution 1
+
+// const names=[]
+// for(let i=0;i<users.length;i++){
+//     names.push(users[i].name)
+// }
+
+// console.log("name of users:",names)
+
+// active name
+
+// const names=[]
+// for(let i=0;i<users.length;i++){
+//     if(users[i].isActive){
+
+//         names.push(users[i].name)
+//     }
+// }
+
+// console.log("name of users:",names)
+
+// using forEach()
+// it does not return any value
+
+// const names=[]
+// users.forEach((user)=>{
+//     if(user.isActive){
+
+//         names.push(user.name)
+//     }
+// })
+// console.log("users name is:",names)
+
+// using map() and method chaining
+
+const names=users.filter((user)=>user.isActive).sort((u1,u2)=>u1.age<u2.age?1:-1).map((user)=>user.name)
+console.log("user name is here:",names)
